b9986f76db0308ebc2a2cfffea0d5b4cb54f3528
vggog <gluhovkich@gmail.com>
2023-12-24 23:36:45 +0500

    Добавить модель заказа

--- src/master/model.py
+++ src/master/model.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Mapped
 
 from src.core.model import BaseModel
@@ -11,3 +13,14 @@ class MasterModel(BaseModel):
     phone: Mapped[str]
     password: Mapped[str]
     salt: Mapped[str]
+
+
+class RepairOrderModel(BaseModel):
+    __tablename__ = "repair_orders"
+
+    customer_full_name: Mapped[str]
+    customer_phone_number: Mapped[str]
+    phone_model: Mapped[str]
+    imei: Mapped[str]
+    defect: Mapped[str]
+    note: Mapped[Optional[str]]
